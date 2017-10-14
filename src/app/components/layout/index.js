899f4902b97e6ca2d7d2b707cc9fdd925117fcce
Jose Centeno <jmcenteno@gmail.com>
2017-10-13 22:15:03 -0400

    put all the pieces together

--- src/app/components/layout/index.js
+++ src/app/components/layout/index.js
@@ -1,11 +1,34 @@
 // page components
-
+import Header from '../header';
+import Badge from '../badge';
+import Rankings from '../rankings';
+import Advertisement from '../advertisement';
 
 // page template
 import template from './template.njk';
 
-export default function Layout(data) {
+/**
+ * Main application layout
+ * @param {object} data An object that represents a group of universities
+ */
+function Layout(data) {
 
-  return template.render({});
+  return template.render({
+    header: Header({
+      title: data.ranking,
+      description: data.description
+    }),
+    badge: Badge({
+      src: data.badge,
+      alt: data.ranking
+    }),
+    rankings: Rankings(data.rankings),
+    advertisement: Advertisement({
+      src: 'http://placehold.it/600x500?text=dummy+ad',
+      alt: ''
+    })
+  });
 
 }
+
+export default Layout;
