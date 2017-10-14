cdb4bfe102179a46b0713c957350907151de9071
Jose Centeno <jmcenteno@gmail.com>
2017-10-13 22:14:07 -0400

    advertisement component

---
+++ src/app/components/advertisement/index.js
@@ -0,0 +1,22 @@
+import template from './template.njk';
+
+/**
+ * Advertisement component
+ * @param {object} args { src, alt }
+ */
+function Advertisement(args = { src: '', alt: '' }) {
+
+  if (!args) {
+    throw new Error('Invalid argument');
+  }
+
+  return template.render({
+    img: {
+      src: args.src || 'http://placehold.it/600x500?text=dummy+ad',
+      alt: args.alt || ''
+    }
+  });
+
+}
+
+export default Advertisement;
