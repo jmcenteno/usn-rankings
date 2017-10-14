98a4830e3ca24b75b32142cc8dc249a163d0e655
Jose Centeno <jmcenteno@gmail.com>
2017-10-13 20:35:46 -0400

    main layout component

---
+++ src/app/components/layout/index.js
@@ -0,0 +1,11 @@
+// page components
+
+
+// page template
+import template from './template.njk';
+
+export default function Layout(data) {
+
+  return template.render({});
+
+}
---
+++ src/app/index.js
@@ -0,0 +1,4 @@
+import Layout from './components/layout';
+import data from '../data/data-1.json';
+
+export default Layout(data);
