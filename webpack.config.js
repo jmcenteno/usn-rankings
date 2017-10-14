6b8c1c5e0d05999a4963208d36feca5b4d5bc6ed
Jose Centeno <jmcenteno@gmail.com>
2017-10-14 14:41:08 -0400

    changed babel presets to latest version

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel', // 'babel-loader' is also a valid name to reference
         query: {
-          presets: ['es2015']
+          presets: ['env']
         }
       },
       {
